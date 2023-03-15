10d54181027b8ddacd63118a915d40046e05833e
Beomchu <izekfzha@gmail.com>
2023-03-15 23:58:07 +0900

    start

---
+++ src/main/java/com/example/beomchulog/BeomchulogApplication.java
@@ -0,0 +1,13 @@
+package com.example.beomchulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeomchulogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeomchulogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beomchulog/BeomchulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beomchulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeomchulogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
